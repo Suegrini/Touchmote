25618ccb31b5018613935f463ceead29f0c2c9df
Sergio Figueroa <sergioarmandoft@gmail.com>
2024-11-11 16:50:35 -0800

    Install all needed instances of the Vmulti driver. Removed ScpBusDriver and TUIO service installation. Removes all devices except mouse and keyboard.

--- DriverInstall/MainWindow.xaml.cs
+++ DriverInstall/MainWindow.xaml.cs
@@ -22,10 +22,6 @@ namespace DriverInstall
 {
     public partial class MainWindow : Window
     {
-        private string etd_ServiceName = "Tuio-To-vmulti-Device1";
-        private string etd_ServiceFilename = "Driver\\tuio_vmulti_service.exe";
-        private string edt_dataFolder = "C:\\Users\\AppData\\TUIO-To-Vmulti\\Data\\";
-
         private bool shutdown = false;
 
         public MainWindow()
@@ -45,16 +41,6 @@ namespace DriverInstall
                     this.installVmultiDriverComplete();
                 }
 
-                if (Environment.GetCommandLineArgs().Contains("-service"))
-                {
-                    this.installTuioServiceComplete();
-                }
-
-                if (Environment.GetCommandLineArgs().Contains("-scpbus"))
-                {
-                    this.installScpBusDriverComplete();
-                }
-
                 if (Environment.GetCommandLineArgs().Contains("-certificate"))
                 {
                     this.installCert();
@@ -63,19 +49,9 @@ namespace DriverInstall
             else if (Environment.GetCommandLineArgs().Contains("-uninstall"))
             {
 
-                if (Environment.GetCommandLineArgs().Contains("-service"))
-                {
-                    this.uninstallTuioServiceComplete();
-                }
-
                 if (Environment.GetCommandLineArgs().Contains("-vmulti"))
                 {
                     this.uninstallVmultiDriverComplete();
-                }
-
-                if (Environment.GetCommandLineArgs().Contains("-scpbus"))
-                {
-                    this.uninstallScpBusDriverComplete();
                 }
 
                 if (Environment.GetCommandLineArgs().Contains("-certificate"))
@@ -99,54 +75,24 @@ namespace DriverInstall
 
         private void installAll()
         {
-            this.installAllDriversComplete();
+            this.installVmultiDriverComplete();
             this.uninstallCert();
             this.installCert();
         }
 
         private void uninstallAll()
         {
-            this.uninstallAllDriversComplete();
+            this.uninstallVmultiDriverComplete();
             this.uninstallCert();
-        }
-
-        private void installAllDriversComplete()
-        {
-            this.installVmultiDriverComplete();
-            this.installScpBusDriverComplete();
-            this.installTuioServiceComplete();
-            //this.store_settings();
-            //this.uninstall_service(etd_ServiceName, etd_ServiceFilename);
-            //this.install_service(etd_ServiceName, etd_ServiceFilename, "3333");
-            //this.give_service_permissions(etd_ServiceName);
         }
 
         private void installVmultiDriverComplete()
         {
             this.uninstallVmultiDriver();
             this.uninstallVmultiDriver();
-            this.installVmultiDriver();
+            this.installVmultiDrivers();
 
-            this.removeAllButTouch();
-        }
-
-        private void installTuioServiceComplete()
-        {
-            this.store_settings();
-            this.uninstall_service(etd_ServiceName, etd_ServiceFilename);
-            this.install_service(etd_ServiceName, etd_ServiceFilename, "3333");
-            this.give_service_permissions(etd_ServiceName);
-        }
-
-        private void uninstallTuioServiceComplete()
-        {
-            this.uninstall_service(etd_ServiceName, etd_ServiceFilename);
-        }
-
-        private void installScpBusDriverComplete()
-        {
-            this.uninstallScpBusDriver();
-            this.installScpBusDriver();
+            this.removeAllButMKB();
         }
 
         private void installCert()
@@ -183,77 +129,44 @@ namespace DriverInstall
             store.Close();
         }
 
-        private void uninstallAllDriversComplete()
-        {
-            uninstallVmultiDriverComplete();
-            uninstallScpBusDriverComplete();
-            uninstallTuioServiceComplete();
-        }
-
         private void uninstallVmultiDriverComplete()
         {
             this.uninstallVmultiDriver();
             this.uninstallVmultiDriver();
         }
 
-        private void uninstallScpBusDriverComplete()
-        {
-            this.uninstallScpBusDriver();
-        }
-
-        private void installVmultiDriver()
+        private void installVmultiDrivers()
         {
             try
             {
-                //Devcon install vmultia.inf ecologylab\vmultia
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo();
+                string[] drivers = { "vmultia", "vmultib", "vmultic", "vmultid" };
 
-                procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
+                foreach (string driver in drivers)
+                {
+                    System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo
+                    {
+                        WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\",
+                        FileName = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\devcon",
+                        Arguments = $"install {driver}.inf ecologylab\\{driver}",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
-                procStartInfo.Arguments = "install vmultia.inf ecologylab\\vmultia";
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                procStartInfo.CreateNoWindow = true;
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-                string result = proc.StandardOutput.ReadToEnd();
-                consoleLine(result);
-                proc.WaitForExit();
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process
+                    {
+                        StartInfo = procStartInfo
+                    };
+
+                    proc.Start();
+                    string result = proc.StandardOutput.ReadToEnd();
+                    Console.WriteLine(result);
+                    proc.WaitForExit();
+                }
             }
-            catch (Exception objException)
+            catch (Exception ex)
             {
-                consoleLine(objException.Message);
-            }
-        }
-
-        private void installScpBusDriver()
-        {
-            try
-            {
-                //Devcon install vmultia.inf ecologylab\vmultia
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo();
-
-                procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
-
-                procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
-                procStartInfo.Arguments = "install ScpVBus.inf root\\ScpVBus";
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                procStartInfo.CreateNoWindow = true;
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-                string result = proc.StandardOutput.ReadToEnd();
-                consoleLine(result);
-                proc.WaitForExit();
-            }
-            catch (Exception objException)
-            {
-                consoleLine(objException.Message);
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -286,200 +199,52 @@ namespace DriverInstall
             }
         }
 
-        private void uninstallScpBusDriver()
+        private void removeAllButMKB()
         {
-            try
-            {
-                //Devcon remove *multi*
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo();
+            int[] drivers = { 1, 2, 4, 5, 6 };
 
-                procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
-
-                procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
-                procStartInfo.Arguments = "remove *Scp*";
-
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                procStartInfo.CreateNoWindow = true;
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-                string result = proc.StandardOutput.ReadToEnd();
-                consoleLine(result);
-                proc.WaitForExit();
-            }
-            catch (Exception objException)
-            {
-                consoleLine(objException.Message);
-            }
-        }
-
-        private void removeAllButTouch()
-        {
-            for (int i = 2; i <= 7; i++)
+            foreach (int i in drivers)
             {
                 try
                 {
-                    //Devcon remove *multi*
-                    System.Diagnostics.ProcessStartInfo procStartInfo =
-                        new System.Diagnostics.ProcessStartInfo();
+                    // Setup the process start info
+                    System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo
+                    {
+                        WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\",
+                        FileName = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\devcon",
+                        Arguments = "disable *vmulti*COL0" + i + "*",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                    procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
+                    // Create and start the process
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process
+                    {
+                        StartInfo = procStartInfo
+                    };
 
-                    procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
-                    procStartInfo.Arguments = "disable *vmulti*COL0"+i+"*";
-
-                    procStartInfo.RedirectStandardOutput = true;
-                    procStartInfo.UseShellExecute = false;
-                    procStartInfo.CreateNoWindow = true;
-                    System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                    proc.StartInfo = procStartInfo;
                     proc.Start();
                     string result = proc.StandardOutput.ReadToEnd();
-                    consoleLine(result);
+                    Console.WriteLine(result);
                     proc.WaitForExit();
                 }
-                catch (Exception objException)
+                catch (Exception ex)
                 {
-                    consoleLine(objException.Message);
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
-        //As described here
-        //http://stackoverflow.com/questions/4436558/start-stop-a-windows-service-from-a-non-administrator-user-account
-        private void give_service_permissions(string service_name)
-        {
 
-            //LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList RegistryKey
-            string keyName = "LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
-            RegistryKey profileList = Registry.LocalMachine.OpenSubKey(keyName);
-            WindowsIdentity user = WindowsIdentity.GetCurrent();
-            SecurityIdentifier sid = user.User;
-            consoleLine(sid.Value);
-
-            string service_permissions = sdshow_service(service_name);
-            string permission_string = "(A;;RPWPCR;;;" + sid.Value + ")";
-            consoleLine(permission_string);
-            service_permissions = service_permissions.Replace(Environment.NewLine, "");
-            service_permissions = service_permissions.Replace(permission_string,"");
-            service_permissions = service_permissions.Replace("S:", permission_string + "S:");
-
-            sdset_service(service_name, service_permissions);
-        }
-
-        //sc sdshow Tuio-To-vmulti-Device1
-        public string sdshow_service(string service_name)
-        {
-            try
-            {
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo();
-
-                procStartInfo.FileName = "sc";
-                procStartInfo.Arguments = "sdshow " + service_name;
-                consoleLine("running " + procStartInfo.FileName + " " + procStartInfo.Arguments);
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                procStartInfo.CreateNoWindow = true;
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-                return proc.StandardOutput.ReadToEnd();
-            }
-            catch (Exception objException)
-            {
-                consoleLine(objException.Message);
-                return "";
-            }
-        }
-
-        public void sdset_service(string service_name, string permission_string)
-        {
-            try
-            {
-                System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo();
-
-                procStartInfo.FileName = "sc";
-                procStartInfo.Arguments = "sdset " + service_name + " " + permission_string;
-                consoleLine("running " + procStartInfo.FileName + " " + procStartInfo.Arguments);
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                procStartInfo.CreateNoWindow = true;
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
-            }
-            catch (Exception objException)
-            {
-                consoleLine(objException.Message);
-            }
-        }
-
-        private void store_settings()
-        {
-
-            System.IO.Directory.CreateDirectory(this.edt_dataFolder);
-
-            System.IO.File.WriteAllText(this.edt_dataFolder + "tuioport1.txt", "3333");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "inverthorizontal1.txt", "false");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "invertverticle1.txt", "false");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "swapxy1.txt", "false");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "xrange_min1.txt", "0");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "xrange_max1.txt", "1");
-
-            System.IO.File.WriteAllText(this.edt_dataFolder + "yrange_min1.txt", "0");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "yrange_max1.txt", "1");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "x01.txt", "0");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "y01.txt", "0");
-            System.IO.File.WriteAllText(this.edt_dataFolder + "service1.txt", "");
-
-        }
-
-        private void install_service(string service_name, string file_name, string port)
-        {
-            consoleLine("Installing service : " + service_name + " : " + file_name + " , port : " + port);
-            //status = sc.Status.ToString();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = file_name;
-            startInfo.Arguments = "install " + port;
-            startInfo.UseShellExecute = true;
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.CreateNoWindow = true;
-            using (Process exeProcess = Process.Start(startInfo))
-            {
-                exeProcess.WaitForExit();
-            }
-
-        }
-
-        private void uninstall_service(string service_name, string file_name)
-        {
-            consoleLine("Uninstalling service : " + service_name + " : " + file_name);
-            //Process Process_Remove = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = file_name;
-            startInfo.Arguments = "remove " + "3";
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = true;
-
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-
-            using (Process exeProcess = Process.Start(startInfo))
-            {
-                exeProcess.WaitForExit();
-            }
-        }
 
         private void btnInstall_Click(object sender, RoutedEventArgs e)
         {
-            this.installAllDriversComplete();
+            this.installVmultiDriverComplete();
         }
 
         private void btnUninstall_Click(object sender, RoutedEventArgs e)
         {
-            this.uninstallAllDriversComplete();
+            this.uninstallVmultiDriverComplete();
         }
 
     }
